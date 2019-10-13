69a8c173ee8b318eff0bb26af1e0ba67dee628a9
PrachiSinghal86 <prachisinghals076@gmail.com>
2019-10-13 23:30:41 +0530

    Create VOWELTB.cpp

---
+++ VOWELTB.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	char C;
+	cin>>C;
+	if(C=='A'||C=='I'||C=='E'||C=='O'||C=='U')
+	cout<<"Vowel"<<"\n";
+	else
+	cout<<"Consonant";
+	return 0;
+}
